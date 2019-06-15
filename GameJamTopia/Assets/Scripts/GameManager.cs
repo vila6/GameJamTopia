8acd84486d497c83f331d3be5e1e865ef0ed1d02
vila6 <mrvila@esei.uvigo.es>
2019-06-15 04:20:55 +0200

    Design y tal

---
+++ GameJamTopia/Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public enum GameState { PLAYING, PAUSED, CINEMATIC }
+    GameState state = GameState.CINEMATIC;
+    public int score, highScore;
+    bool isPaused;
+
+    #region Singleton
+    public static GameManager instance = null;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(this.gameObject);
+
+        Object.DontDestroyOnLoad(this.gameObject);
+    }
+    #endregion
+
+    private void Update()
+    {
+        // Detect pause input
+        if (Input.GetButtonDown("Pause"))
+        {
+            Pause();
+        }
+    }
+    public void Pause()
+    {
+        Debug.Log("Game paused");
+        state = GameState.PAUSED;
+        Time.timeScale = 0;
+    }
+
+    // End pause
+    public void Continue()
+    {
+        Debug.Log("Game despaused (?)");
+        state = GameState.PLAYING;
+        Time.timeScale = 1;
+    }
+}
---
+++ GameJamTopia/Assets/Squid.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Squid : MonoBehaviour
+{
+    public int life = 2;
+    public int inkShots = 3;
+    public float shootMaxCooldown = 1, shootCooldown = 1;
+    public float movementMaxCooldown = 3, movementCooldown = 3;
+
+    public GameObject inkProjectile;
+    public GameObject waterProjectile;
+
+    Transform target;
+
+    private void Start()
+    {
+        target = PlayerController.instance.transform;
+    }
+
+    void Update()
+    {
+        if ( shootCooldown <= 0 )
+        {
+            // target = player
+            Shoot();
+            shootCooldown = shootMaxCooldown;
+        }
+        else
+        {
+            shootCooldown -= Time.deltaTime;
+        }
+    }
+
+    public void Shoot()
+    {
+        //TODO lanzar proyectiles desde un shootpoint
+        GameObject shot;
+        if (inkShots > 0 && Random.Range(1, 2) == 1)
+        {
+            shot = Instantiate(inkProjectile);
+            inkShots--;
+        }
+        else
+        {
+            shot = Instantiate(waterProjectile);
+        }
+        shot.transform.LookAt(target);
+    }
+
+    // The enemy recieves an ammount of damage
+    public void Hurt(int damage)
+    {
+        life -= damage;
+        if (life <= 0)
+            Die();
+    }
+
+    // The enemy dies
+    public void Die()
+    {
+        //TODO spawnear pickup de tinta
+        Destroy(gameObject);
+    }
+}
