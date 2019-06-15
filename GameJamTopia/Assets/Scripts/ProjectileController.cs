6ee7f3fce1af061c4168bd97f1da630a8676657c
Adrián <adriang.gtb@gmail.com>
2019-06-15 04:17:36 +0200

    Ahora los proyectiles salen desde dentro del pincel

--- GameJamTopia/Assets/Scripts/ProjectileController.cs
+++ GameJamTopia/Assets/Scripts/ProjectileController.cs
@@ -6,12 +6,22 @@ public class ProjectileController : MonoBehaviour
 {
     public Vector3 direction;
     public float velocity = 10f;
+    public float delayUntilTriggerWorking;
+
     void Start()
     {
         this.transform.rotation = Quaternion.Euler(0, 0, 90);
         this.GetComponent<Rigidbody>().velocity = direction * velocity;
         
         Destroy(this.gameObject, 10f);
+
+        StartCoroutine(DelayTriggerForSpawn());
+    }
+
+    private IEnumerator DelayTriggerForSpawn()
+    {
+        yield return new WaitForSeconds(delayUntilTriggerWorking);
+        this.GetComponent<Collider>().enabled = true;
     }
 
     void OnTriggerEnter(Collider collider)
