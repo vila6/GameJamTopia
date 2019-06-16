ac2ffe45b5db11fdb691ee707749d2e40ac972bd
Agonesc <angelgonesc@gmail.com>
2019-06-16 05:21:07 +0200

    PickUp ya no es un cubo Y camera shake dentro
    Script de Camera Shake y el PickUp con particulas

---
+++ GameJamTopia/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+	public Transform camTransform;
+	
+	// El tiempo que quieres que haga el shake
+	public float shakeDuration = 0f;
+	
+	// Valores para el meneo de la camara
+	public float shakeAmount = 0.7f;
+	public float decreaseFactor = 1.0f;
+	
+	Vector3 originalPos;
+	
+	void Awake()
+	{
+		if (camTransform == null)
+		{
+			camTransform = GetComponent(typeof(Transform)) as Transform;
+		}
+	}
+	
+	void OnEnable()
+	{
+		originalPos = camTransform.localPosition;
+	}
+
+	void Update()
+	{
+		if (shakeDuration > 0)
+		{
+			camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+			
+			shakeDuration -= Time.deltaTime * decreaseFactor;
+		}
+		else
+		{
+			shakeDuration = 0f;
+			camTransform.localPosition = originalPos;
+		}
+	}
+}
